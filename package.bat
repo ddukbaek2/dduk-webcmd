60b2157b0b73c8632434f521b178b09722a4d663
ddukbaek2 <dagraac@naver.com>
2024-08-04 22:39:17 +0900

    수정.

--- package.bat
+++ package.bat
@@ -9,21 +9,3 @@ if not defined IS_PACKAGE (
 	echo This batch file cannot be executed.
 	exit /b 1
 )
-
-:: BPY 라이브러리 다운로드.
-if not exist "%BPYFILEPATH%" (
-	echo "Not Exists '%BPYFILEPATH%'"
-	curl -o "%BPYFILEPATH%" "%BPYFILEURL%"
-)
-
-:: MATHUTILS 라이브러리 다운로드.
-if not exist "%MATHUTILSFILEPATH%" (
-	echo "Not Exists '%MATHUTILSFILEPATH%'"
-	curl -o "%MATHUTILSFILEPATH%" "%MATHUTILSFILEURL%"
-)
-
-:: BPY 라이브러리 설치.
-python -m pip install "%BPYFILEPATH%" >nul 2>nul
-
-:: MATHUTILS 라이브러리 설치.
-python -m pip install "%MATHUTILSFILEPATH%" >nul 2>nul
